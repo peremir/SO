d2cddc234924a81175739ace104894c9d9a3530a
peremir <57814247+peremir@users.noreply.github.com>
2021-11-18 00:43:29 +0100

    Update words.c

--- Sessio 2/words.c
+++ Sessio 2/words.c
@@ -4,14 +4,15 @@
 #include "mis_funciones.h"
 
 int main (int argc, char *argv[]) {
-	if (argc == 1){
-	usage_words();
-	return 0;
+	if (argc == 1) {
+		usage_words();
+		return 0;
 	}
 	char buf[80]; 
 	int c = 0;
+	
 	for (int i = 1; i < strlen(argv[1]); i++) {
-      		if (argv[1][i] == ' ' || argv[1][i+1] == '\0' ||  argv[1][i] == ',' || argv[1][i] == '.') ++c;
+  	if (argv[1][i] == ' ' || argv[1][i+1] == '\0' ||  argv[1][i] == ',' || argv[1][i] == '.') ++c;
 	}
 	sprintf (buf, "%d palabras\n", c);
 	write (1, buf, strlen (buf));
